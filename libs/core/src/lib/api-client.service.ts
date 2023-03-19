542967a9f75ca2a7c5a993fe27e66fb4882e905b
Pavel Bozhok <pavel.bozhok@scaleo.io>
2023-03-19 12:24:17 +0200

    refactor api-client.service.ts

--- libs/core/src/lib/api-client.service.ts
+++ libs/core/src/lib/api-client.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ApiClientService {
-
   get(): Observable<string> {
     console.log('run api from public lib');
+
     return of('test');
   }
 }
