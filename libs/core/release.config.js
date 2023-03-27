09389668fac3fe5d9c03bed9f400f8a8e4da500d
Pavel Bozhok <pavel.bozhok@scaleo.io>
2023-03-27 10:03:09 +0300

    update

--- libs/core/release.config.js
+++ libs/core/release.config.js
@@ -6,8 +6,6 @@ module.exports = {
   pkgRoot: `dist/${srcRoot}`,
   tagFormat: name + '-v${version}',
   commitPaths: [`${srcRoot}/*`],
-  branch: 'main',
-  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
